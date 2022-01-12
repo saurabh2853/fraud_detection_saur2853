1fcdaba6434bec5bdfe961b7d1119f12b8c69ca5
saur2853 <saurabh.2853@gmail.com>
2022-01-12 16:10:08 +0530

    added a new function sum

---
+++ data_processing.py
@@ -0,0 +1,7 @@
+print("adding all the data processing things related to fraud detection")
+a=3
+b=4
+def sum(a,b):
+    c=a+b
+    return c
+print("added a function isn't correct and I need to see how this can be reverted to previous version")
